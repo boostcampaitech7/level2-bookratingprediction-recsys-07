d59e76aaa2e57bfac118e3c67519a2fb433f74db
Durhamfighter <sinseunghun708@gmail.com>
2024-11-06 14:47:19 +0000

    Feat: Add data_utils function
    #5

---
+++ src/preprocessor/data_utils.py
@@ -0,0 +1,28 @@
+import regex
+import pandas as pd
+import numpy as np
+
+def split_location(x: str) -> list:
+    '''
+    Parameters
+    ----------
+    x : str
+        location 데이터
+
+    Returns
+    -------
+    res : list
+        location 데이터를 나눈 뒤, 정제한 결과를 반환합니다.
+        순서는 country, state, city, ... 입니다.
+    '''
+    res = x.split(',')
+    res = [i.strip().lower() for i in res]
+    res = [regex.sub(r'[^a-zA-Z/ ]', '', i) for i in res]  # remove special characters
+    res = [i if i not in ['n/a', ''] else np.nan for i in res]  # change 'n/a' into NaN
+    res.reverse()  # reverse the list to get country, state, city, ... order
+
+    for i in range(len(res)-1, 0, -1):
+        if (res[i] in res[:i]) and (not pd.isna(res[i])):  # remove duplicated values if not NaN
+            res.pop(i)
+
+    return res
